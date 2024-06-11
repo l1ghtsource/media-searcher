4a5881b36e049262bbeb25ea84a82a21bcf9090b
lightsource <m2302070@edu.misis.ru>
2024-06-11 14:54:55 +0300

    del comms

--- ml/text2minilm_model.py
+++ ml/text2minilm_model.py
@@ -5,41 +5,17 @@ from transformers import AutoModel, AutoTokenizer
 
 class Text2MiniLM:
     def __init__(self):
-        '''
-        Initializes the Text2MiniLM class.
-
-        This method sets up the device (GPU if available, otherwise CPU), loads the pretrained MiniLM model and tokenizer.
-        '''
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2').to(self.device)
         self.tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
 
     def mean_pooling(self, model_output, attention_mask):
-        '''
-        Applies mean pooling to the token embeddings to obtain sentence embeddings.
-
-        Args:
-            model_output (torch.Tensor): The output tensor from the model.
-            attention_mask (torch.Tensor): The attention mask tensor.
-
-        Returns:
-            torch.Tensor: The sentence embeddings obtained by mean pooling.
-        '''
         token_embeddings = model_output[0]
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
 
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     def get_lm_embedding(self, text):
-        '''
-        Computes the sentence embeddings for the input text using the MiniLM model.
-
-        Args:
-            text (str): The input text to be embedded.
-
-        Returns:
-            torch.Tensor: A tensor containing the normalized sentence embeddings (size 384).
-        '''
         encoded_input = self.tokenizer([text], padding=True, truncation=True, return_tensors='pt').to(self.device)
 
         with torch.no_grad():
--- ml/whisper_model.py
+++ ml/whisper_model.py
@@ -8,15 +8,6 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
 
 class WhisperModel:
     def __init__(self):
-        '''
-        Initializes the WhisperModel class.
-
-        This method sets up the device (GPU if available, otherwise CPU), loads the pretrained Whisper model and processor, 
-        and initializes the voice activity detection (VAD) model.
-
-        Args:
-            model_id (str): The identifier of the pretrained Whisper model to be used.
-        '''
         flag = torch.cuda.is_available()
 
         self.model_id = 'openai/whisper-medium' if flag else 'openai/whisper-base'
@@ -44,15 +35,6 @@ class WhisperModel:
             os.makedirs(os.path.join(working_dir, 'tmp'))
 
     def process_sample(self, filename):
-        '''
-        Processes an audio sample from a video file to extract and transcribe speech.
-
-        Args:
-            filename (str): The path to the video file to be processed.
-
-        Returns:
-            str: The transcribed text from the audio.
-        '''
         print(filename)
 
         try:
